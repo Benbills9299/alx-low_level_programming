5c5e39a584b7b874d04bc34af77abf1f30d36ec1
benbills <onobiagodspower@gmail.com>
2023-02-16 20:24:35 -0800

    adding my first c program file 4-puts it prints Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - indicates our Entry point
+ *
+ * Return end program with the value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
