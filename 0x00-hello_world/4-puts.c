72a12c74d73d21d1aa1aa9fa39b6fc857a70035a
benbills <onobiagodspower@gmail.com>
2023-02-16 20:49:00 -0800

    editing 4-put c program file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - indicates our Entry point
- * discription: the program prints programmin is like a building puzzle
- * Return program end with 0 value
- *
+ *main- A program that uses puts to print a string
+ *Return: ends with 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
