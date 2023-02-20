4595efae808d963cbe521523e965f6282f5566de
benbills <onobiagodspower@gmail.com>
2023-02-19 19:25:02 -0800

    adding a new c program to print last number in a varaible n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	k = n % 10;
 	if (k > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n", n, k);
+		printf("Last digit of %d is %d and is greater than 5\n", n, k);
 	}
 	else if (k == 0)
 	{
