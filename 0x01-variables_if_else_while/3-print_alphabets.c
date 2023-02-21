83a74ddd1d3e9cd2b2b9d2a4a07a4ceab247cff1
benbills <onobiagodspower@gmail.com>
2023-02-20 19:13:22 -0800

    adding a new c program that prints uppercase letter and lowercase letter using only putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz"
+	char BIGALP[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	int o;
+
+	for (i = 0; i <= 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	for (o = 0; o <= 27; o++)
+	{
+		putchar(BIGALP[o]);
+	}
+	putchar('\n');
+	return (0);
+}
