067d8d25d1b736779fb1a0458ebe73c516c16844
benbills <onobiagodspower@gmail.com>
2023-02-28 14:36:17 -0800

    edited

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,11 @@
-int _putchar(char c);
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,4 +16,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s)
+int _atoi(char *s);
+int _putchar(char c);
+
+#endif /* MIAN_H_INCLUDED */
