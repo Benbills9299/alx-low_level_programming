4799c9663fefa9876fb04ef3093d31d2d89ff653
benbills <onobiagodspower@gmail.com>
2023-03-30 04:05:11 -0700

    adding edited files

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,24 +1,20 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - computes the length of the liked list..
- * @h: a pointer to the list to iterato to.
- * Return: the number of nodes
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	const list_t *tmp;
-	size_t i = 0;
+	size_t n = 0;
 
-	tmp = h;
-	if (tmp)
-		tmp = h;
-	while (tmp)
+	while (h)
 	{
-		i++;
-		tmp = tmp->next;
+		n++;
+		h = h->next;
 	}
-	return (i);
+	return (n);
 }
